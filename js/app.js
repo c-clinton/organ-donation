65d23ba123f15b394caf171ccf75d9460fe8cef8
CJ <c_clinton2@fanshaweonline.ca>
2017-02-16 18:42:53 -0500

    About page nearly finished; video function complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-$(document).foundation()
+
+$(document).foundation();
+
+(function() {
+
+	"use strict";
+
+	var vid = document.querySelector('#introvid');
+	var header = document.querySelector('.largeimgcon');
+	var skip = document.querySelector('#skip');
+
+	function showheader(evt){
+
+	evt.preventDefault();
+	vid.classList.add("hidden");
+	vid.classList.remove("visible");
+	header.classList.add("visible");
+	header.classList.remove("hidden");
+	}
+
+	skip.addEventListener("click", showheader, false);
+	vid.addEventListener("ended", showheader, false);
+
+
+})();
