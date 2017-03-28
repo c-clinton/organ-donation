722e709752375c505396d6078e615ca0ad93e277
c-clinton <c_clinton2@fanshaweonline.ca>
2017-03-28 02:35:12 -0400

    Add files via upload

--- admin_createuser.php
+++ admin_createuser.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors',1);
     error_reporting(E_ALL);
 	
-	require_once("admin/phpscripts/init.php");
+	require_once("phpscripts/init.php");
 	//confirm_logged_in();
 	
 	if(isset($_POST['submit'])) {
@@ -10,7 +10,7 @@
 		$fname = trim($_POST['fname']);
 		$lname = trim($_POST['lname']);
 		$username = trim($_POST['username']);
-		$email = trim($_POST['email']);
+		$password = trim($_POST['password']);
 		$level = trim($_POST['lvllist']);
 		if(empty($level)) { //checks if level is empty
 		
@@ -21,7 +21,7 @@
 		}else{
 			
 			//echo "Level selected.";
-			$result = createUser($fname, $lname, $username, $email, $level);
+			$result = createUser($fname, $lname, $username, $password, $level);
 			$message = $result;
 		}
 	}
@@ -33,62 +33,32 @@
 <head>
 <meta charset="UTF-8">
 <title>Create User</title>
-<link rel="stylesheet" href="css/foundation.min.css">
-<link rel="stylesheet" href="css/app.css">
 </head>
 
 <body>
-
-<div class="container">
-
-<section class="whitebox">
-
-<h1>Create User</h1>
-
-<div class="formsection">
-
-<h2 class="hidden">Please fill out the fields below.</h2>
-
 <?php if(!empty($message)){echo $message;} ?>
-
-
 <form action ="admin_createuser.php" method="post">
-
-<div class="stack">
-
 <label>First Name:</label>
 <input type="text" name="fname" value="<?php if(!empty($fname)){echo $fname;} ?>">
 <label>Last Name:</label>
 <input name="lname" type="text" value="<?php if(!empty($lname)){echo $lname;} ?>">
-
-</div>
-
-<div class="stack">
 <label>Username:</label>
 <input name="username" type="text" value="<?php if(!empty($username)){echo $username;} ?>">
-<label>Email:</label>
-<input name="email" type="text" value="<?php if(!empty($email)){echo $email;} ?>">
-</div>
+<label>Password</label>
+<input name="password" type="text" value="<?php if(!empty($password)){echo $password;} ?>">
 
-<div class="center">
+<br></br>
+
 <select name="lvllist">
 	<option value="">Please select User Level...</option>option>
     <option value="2">Web Admin</option>
     <option value="1">Web Master</option>
 </select>
-
-</div>
-</div>
-
-<input class="stack importantbut" type="submit" name="submit" value="Create User">
+<br><br>
+<input type="submit" name="submit" value="Create User">
 </form>
-
-
-<a class="stack centertext link" href="admin_index.php">Back to Admin Panel</a>
-
-</section>
-
-</div>
+<br><br>
+<a href="admin_index.php">Back to Admin Panel</a>
 
 </body>
 </html>
--- admin_edituser.php
+++ admin_edituser.php
@@ -2,11 +2,14 @@
 	ini_set('display_errors',1);
     error_reporting(E_ALL);
 
-    require_once("admin/phpscripts/init.php");
+    require_once("phpscripts/init.php");
 	//confirm_logged_in();
 
-    $id = $_SESSION['user_id'];
-    $populate = getUser($id);
+    $id = $_SESSION['users_id'];
+    //echo $id;
+    //
+    $popForm = getUser($id);
+    //echo $popForm;
 	
 
 	
@@ -15,10 +18,9 @@
 		$fname = trim($_POST['fname']);
 		$lname = trim($_POST['lname']);
 		$username = trim($_POST['username']);
-		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
 		
-		$result = editUser($fname, $lname, $username, $email, $password, $id);
+		$result = editUser($fname, $lname, $username, $password);
 		$message = $result;
 		}
 
@@ -29,60 +31,27 @@
 <head>
 <meta charset="UTF-8">
 <title>Edit User</title>
-<link rel="stylesheet" href="css/foundation.min.css">
-<link rel="stylesheet" href="css/app.css">
 </head>
-
-<body>
-
-<div class="container">
-
-<section class="whitebox">
 
 <h1>Edit User</h1>
 
-<div class="formsection">
-
-<h2 class="hidden">Please fill out the fields below.</h2>
-
+<body>
 <?php if(!empty($message)){echo $message;} ?>
-
-
 <form action ="admin_edituser.php" method="post">
-
-<div class="stack">
-
 <label>First Name:</label>
-<input type="text" name="fname" value="<?php echo $populate['user_fname'];?>">
+<input type="text" name="fname" value="<?php echo $popForm['user_fname'];?>">
 <label>Last Name:</label>
-<input name="lname" type="text" value="<?php echo $populate['user_lname'];?>">
-
-</div>
-
-<div class="stack">
+<input name="lname" type="text" value="<?php echo $popForm['user_lname'];?>">
 <label>Username:</label>
-<input name="username" type="text" value="<?php echo $populate['user_name'];?>">
-<label>Email:</label>
-<input name="email" type="text" value="<?php echo $populate['user_email'];?>">
-</div>
+<input name="username" type="text" value="<?php echo $popForm['user_name'];?>">
+<label>Password</label>
+<input name="password" type="text" value="<?php echo $popForm['user_pass'];?>">
 
-<div class="stack">
-<label>Password:</label>
-<input name="password" type="text" value="<?php echo $populate['user_hash'];?>">
-
-<!--I'd probably have to write a function that would decode encrypted passwords from the salt + hash then re-encrypt the NEW password like I attempted with the login function, but I'll keep this simple for now.-->
-</div>
-</div>
-
-<input class="stack importantbut" type="submit" name="submit" value="Edit User">
+<br><br>
+<input type="submit" name="submit" value="Edit User">
 </form>
-
-
-<a class="stack centertext link" href="admin_index.php">Back to Admin Panel</a>
-
-</section>
-
-</div>
+<br><br>
+<a href="admin_index.php">Back to Admin Panel</a>
 
 </body>
 </html>
